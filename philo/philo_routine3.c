14d7c4cf9c3b592d5eaf32a6e732e6a2db17f120
Joonho Lee <joonhlee@student.42seoul.kr>
2023-06-23 08:27:31 +0900

    remove unnecessary short sleep

--- philo/philo_routine3.c
+++ philo/philo_routine3.c
@@ -6,7 +6,7 @@
 /*   By: joonhlee <joonhlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 07:40:33 by joonhlee          #+#    #+#             */
-/*   Updated: 2023/06/22 18:22:09 by joonhlee         ###   ########.fr       */
+/*   Updated: 2023/06/23 08:24:51 by joonhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,6 @@ int	refresh_unit_time(t_philo *philo, t_timeval time)
 			- get_utime_diff(time, philo->t_last_sleep);
 	t_left_die = philo->share->t_die - get_utime_diff(time, philo->t_last_eat);
 	t_left_min = (philo_min(t_left_state, t_left_die) * 3) / 4;
-	if (philo->status == EATING)
-		t_left_min = philo_min(t_left_min, 3000);
 	if (T_UNIT < t_left_min)
 		return ((int) t_left_min);
 	else
